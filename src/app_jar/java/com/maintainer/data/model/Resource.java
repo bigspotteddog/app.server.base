d6f89a724b65699042d69aed05ab6b09f91670fd
Joseph Cava-Lynch <bink.lynch@gmail.com>
2012-06-29 15:47:47 -0700

    See #137: Added the ability to specify an unsecured server resource.  Right now, a credentials key must be provided as a query parameter which will be used to verify the access to be granted.

--- src/app_jar/java/com/maintainer/data/model/Resource.java
+++ src/app_jar/java/com/maintainer/data/model/Resource.java
@@ -6,4 +6,5 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Resource {
     String name() default "";
+    boolean secured() default true;
 }
--- src/app_jar/java/com/maintainer/data/security/MyCookieAuthenticator.java
+++ src/app_jar/java/com/maintainer/data/security/MyCookieAuthenticator.java
@@ -2,6 +2,7 @@ package com.maintainer.data.security;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.restlet.Context;
 import org.restlet.Request;
 import org.restlet.Response;
@@ -12,20 +13,24 @@ import org.restlet.data.Preference;
 import org.restlet.data.Reference;
 import org.restlet.data.Status;
 import org.restlet.ext.crypto.CookieAuthenticator;
+import org.restlet.resource.ServerResource;
 import org.restlet.security.User;
 
+import com.maintainer.data.model.Resource;
 import com.maintainer.data.router.WebSwitch;
 import com.maintainer.data.security.model.Function;
 import com.maintainer.data.security.model.Role;
 import com.maintainer.util.Utils;
 
 public class MyCookieAuthenticator extends CookieAuthenticator {
-    public MyCookieAuthenticator(Context context, boolean optional, String realm, byte[] encryptSecretKey) {
+    private static final Logger log = Logger.getLogger(MyCookieAuthenticator.class.getName());
+
+    public MyCookieAuthenticator(final Context context, final boolean optional, final String realm, final byte[] encryptSecretKey) {
         super(context, optional, realm, encryptSecretKey);
     }
 
     @Override
-    protected CookieSetting getCredentialsCookie(Request request, Response response) {
+    protected CookieSetting getCredentialsCookie(final Request request, final Response response) {
         CookieSetting credentialsCookie = response.getCookieSettings()
                 .getFirst(getCookieName());
 
@@ -40,14 +45,29 @@ public class MyCookieAuthenticator extends CookieAuthenticator {
     }
 
     @Override
-    protected boolean authenticate(Request request, Response response) {
-        boolean authenticated = super.authenticate(request, response);
+    protected boolean authenticate(final Request request, final Response response) {
+        boolean authenticated = false;
+
+        final Class<? extends ServerResource> resourceClass = Utils.getTargetServerResource((WebSwitch) getApplication(), request);
+        final Resource annotation = resourceClass.getAnnotation(Resource.class);
+
+        if (annotation != null && !annotation.secured()) {
+            final CookieSetting credentialsCookie = new CookieSetting(getCookieName(), null);
+            final String credentials = request.getResourceRef().getQueryAsForm().getFirstValue("key");
+            if (credentials != null) {
+                credentialsCookie.setValue(credentials);
+                request.getCookies().add(credentialsCookie);
+            }
+        }
+
+        authenticated = super.authenticate(request, response);
+
         if (!authenticated) {
             boolean json = false;
 
-            List<Preference<MediaType>> mediaTypes = Request.getCurrent().getClientInfo().getAcceptedMediaTypes();
-            for (Preference<MediaType> t : mediaTypes) {
-                MediaType type = t.getMetadata();
+            final List<Preference<MediaType>> mediaTypes = Request.getCurrent().getClientInfo().getAcceptedMediaTypes();
+            for (final Preference<MediaType> t : mediaTypes) {
+                final MediaType type = t.getMetadata();
                 if (type.equals(MediaType.APPLICATION_JSON)) {
                     json = true;
                     break;
@@ -64,20 +84,20 @@ public class MyCookieAuthenticator extends CookieAuthenticator {
 
     @SuppressWarnings({ "unused", "unchecked" })
     @Override
-    protected int authenticated(Request request, Response response) {
+    protected int authenticated(final Request request, final Response response) {
         if (!Utils.isInternalRequest(request)) {
-            ClientInfo clientInfo = request.getClientInfo();
-            User user = clientInfo.getUser();
+            final ClientInfo clientInfo = request.getClientInfo();
+            final User user = clientInfo.getUser();
             if (user != null) {
-                String identifier = user.getIdentifier();
+                final String identifier = user.getIdentifier();
 
-                List<org.restlet.security.Role> roles = clientInfo.getRoles();
+                final List<org.restlet.security.Role> roles = clientInfo.getRoles();
 
-                List<Role> groups = (List<Role>) Utils.subrequest((WebSwitch) getApplication(), "roles?users.username=" + user.getIdentifier(), request);
+                final List<Role> groups = (List<Role>) Utils.subrequest((WebSwitch) getApplication(), "roles?users.username=" + user.getIdentifier(), request);
                 if (groups != null) {
-                    for (Role role : groups) {
-                        List<Function> functions = role.getFunctions();
-                        for (Function function : functions) {
+                    for (final Role role : groups) {
+                        final List<Function> functions = role.getFunctions();
+                        for (final Function function : functions) {
                             roles.add(new org.restlet.security.Role(function.getPath(), null));
                         }
                     }
