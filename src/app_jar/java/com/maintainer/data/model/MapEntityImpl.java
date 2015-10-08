c7d09b34081ddfbfd69690ca9932c32e225f6f41
bink.lynch@gmail.com <bink.lynch@gmail.com>
2015-10-08 01:42:31 +0000

    Removed map from the map entity impl so gson will serialize it properly.

--- src/app_jar/java/com/maintainer/data/model/MapEntityImpl.java
+++ src/app_jar/java/com/maintainer/data/model/MapEntityImpl.java
@@ -5,55 +5,42 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
-public class MapEntityImpl extends EntityImpl implements Map<String, Object> {
+import javax.persistence.Transient;
 
-    @NotStored
-    private final LinkedHashMap<String, Object> properties = new LinkedHashMap<String, Object>();
+public class MapEntityImpl extends EntityImpl { //implements Map<String, Object> {
+
+    @NotStored @NotIndexed @Transient
+    transient private final LinkedHashMap<String, Object> properties = new LinkedHashMap<String, Object>();
 
     public boolean isIndexed(final Object key) {
         return true;
     }
 
     @Override
-    public void setId(final String id) {
-        put("id", id);
-    }
-
-    @Override
-    public void setIdentity(final Object identity) {
-        put("identity", identity);
-    }
-
-    @Override
     public void setCursor(final String cursor) {
-        put("cursor", cursor);
+        properties.put("cursor", cursor);
     }
 
-    @Override
     public void clear() {
         properties.clear();
     }
 
 
-    @Override
     public boolean containsKey(final Object key) {
         return properties.containsKey(key);
     }
 
 
-    @Override
     public boolean containsValue(final Object value) {
         return properties.containsValue(value);
     }
 
 
-    @Override
     public Set<java.util.Map.Entry<String, Object>> entrySet() {
         return properties.entrySet();
     }
 
 
-    @Override
     public Object get(final Object key) {
         return properties.get(key);
     }
@@ -62,43 +49,32 @@ public class MapEntityImpl extends EntityImpl implements Map<String, Object> {
         properties.put(key, value);
     }
 
-    @Override
     public boolean isEmpty() {
         return properties.isEmpty();
     }
 
 
-    @Override
     public Set<String> keySet() {
         return properties.keySet();
     }
 
 
-    @Override
     public Object put(final String key, final Object value) {
         return properties.put(key, value);
     }
 
-
-    @Override
     public void putAll(final Map<? extends String, ? extends Object> m) {
         properties.putAll(m);
     }
 
-
-    @Override
     public Object remove(final Object key) {
         return properties.remove(key);
     }
 
-
-    @Override
     public int size() {
         return properties.size();
     }
 
-
-    @Override
     public Collection<Object> values() {
         return properties.values();
     }
